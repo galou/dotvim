86aecdd201e55bb96bfaeea960c2b20aeac38943
Gaël Écorchard <gael@km-robotics.cz>
2026-02-18 10:55:11 +0100

    Add the venn plugin to draw lines and rectangles

---
+++ lua/plugins/venn.lua
@@ -0,0 +1,12 @@
+-- Draw diagrams.
+-- https://github.com/jbyuki/venn.nvim
+-- `:set virtualedit=all` to move also in empty space.
+-- Rectangular selection then :VBox to draw a rectangle (will be a line if thin).
+return {
+{
+  'jbyuki/venn.nvim',
+
+  cmd = {'VBox'},
+
+},
+}
